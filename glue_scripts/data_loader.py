674f7b21b8ac1d19f247ad94f7c2343fe2350a74
Simone <s.zanni4@studenti.unibg.it>
2020-05-22 00:08:30 +0200

    Added data_loader script and lamba function to launch it

---
+++ glue_scripts/data_loader.py
@@ -0,0 +1,96 @@
+
+
+###### TEDx-Load-Aggregate-Model
+
+import sys
+import json
+import pyspark
+from pyspark.sql.functions import col, collect_list, array, lit
+from pyspark.sql.types import StringType, ArrayType, StructType, StructField
+
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+
+
+##### FROM FILES
+tedx_dataset_path = "s3://smarted-data/tedx_dataset.csv"
+
+###### READ PARAMETERS
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+##### START JOB CONTEXT AND JOB
+sc = SparkContext()
+
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+
+#### READ INPUT FILES TO CREATE AN INPUT DATASET
+tedx_dataset = spark.read \
+    .option("header", "true") \
+    .option("multiline", "true") \
+    .option("quote", "\"") \
+    .option("escape", "\"") \
+    .csv(tedx_dataset_path)
+    
+tedx_dataset.printSchema()
+
+
+#### FILTER ITEMS WITH NULL POSTING KEY
+count_items = tedx_dataset.count()
+count_items_null = tedx_dataset.filter("idx is not null").count()
+
+print(f"Number of items from RAW DATA {count_items}")
+print(f"Number of items from RAW DATA with NOT NULL KEY {count_items_null}")
+
+
+## READ TAGS DATASET
+tags_dataset_path = "s3://smarted-data/tags_dataset.csv"
+tags_dataset = spark.read.option("header", "true").csv(tags_dataset_path)
+
+
+# CREATE THE AGGREGATE MODEL, ADD TAGS TO TEDX_DATASET
+tags_dataset_agg = tags_dataset.groupBy(col("idx").alias("idx_ref")).agg(collect_list("tag").alias("tags"))
+tags_dataset_agg.printSchema()
+tedx_dataset_agg = tedx_dataset.join(tags_dataset_agg, tedx_dataset.idx == tags_dataset_agg.idx_ref, "left") \
+    .drop("idx_ref") \
+    .select(col("idx").alias("_id"), col("*")) \
+    .drop("idx") \
+
+tedx_dataset_agg.printSchema()
+
+## READ WATCH NEXT DATASET
+watch_next_dataset_path = "s3://smarted-data/watch_next_dataset.csv"
+watch_next_dataset = spark.read.option("header", "true").csv(watch_next_dataset_path)
+
+
+# CREATE THE AGGREGATE MODEL, ADD WATCH_NEXT TO TEDX_DATASET
+watch_next_dataset_agg = watch_next_dataset.groupBy(col("idx")).agg(collect_list("watch_next_idx").alias("watch_next"))
+tedx_dataset_agg = tedx_dataset_agg.join(watch_next_dataset_agg, tedx_dataset_agg._id == watch_next_dataset_agg.idx, "left") \
+    .drop("idx")
+
+tedx_dataset_agg.printSchema()
+
+
+# WRITE DATA TO DATABASE
+mongo_uri = "mongodb://tcm-shard-00-00-cegfs.mongodb.net:27017,tcm-shard-00-01-cegfs.mongodb.net:27017,tcm-shard-00-02-cegfs.mongodb.net:27017"
+
+mongo_options = {
+    "uri": mongo_uri,
+    "database": "SmarTED",
+    "collection": "talks",
+    "username": " *username* ",
+    "password": " *password* ",
+    "ssl": "true",
+    "ssl.domain_match": "false",
+    "replaceDocument": "false" }
+                     
+tedx_dataset_dynamic_frame = DynamicFrame.fromDF(tedx_dataset_agg, glueContext, "nested")
+glueContext.write_dynamic_frame.from_options(tedx_dataset_dynamic_frame, connection_type="mongodb", connection_options=mongo_options)
